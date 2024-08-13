9ea6cbc5f4b29bed24150fa9bcd8c4fcc850b0ee
EmmanuelAdio <emmanueladio065@gmail.com>
2024-08-13 21:48:44 +0100

    centered text on home page

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -12,17 +12,10 @@ export default function Home() {
   return (
     <>
     <div className="page home container-fluid p-4">
-      <div className="row align-items-center">
-        <div className="col-md-6">
-
-          <div className="textBox Introduction">
-            <h2 className="textBox mb-4">HELLO, MY NAME IS EMMANUEL ADIO</h2>
-            <p>*This is where my short introduction will go*</p>
-          </div>
-          
-        </div>
+      <div className="textBox Introduction">
+        <h2 className="textBox mb-4">HELLO, MY NAME IS EMMANUEL ADIO</h2>
+        <p>*This is where my short introduction will go*</p>
       </div>
-      
     </div>
     <PageLine></PageLine>
     </>
